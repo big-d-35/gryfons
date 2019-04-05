71652a42cd33347caaf16d10176442797109e947
big-d-35 <44974117+big-d-35@users.noreply.github.com>
2019-04-05 18:22:17 +0300

    first js touch
    const pages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
+//КНОПКИ
 const buttonHeaderMenu = document.getElementById('header-button-menu');
 const iconHeaderMenuBack = document.getElementById('header-icon-back');
 const iconHeaderMenu = document.getElementById('header-icon-menu');
 const headerHomeButton = document.getElementById('header-home-button');
 
+//КНОПКИ
+
+
+
+//СТРАНИЦЫ
+const pageHeader = document.getElementById('page-header');
 const pageHome = document.getElementById('page-home');
-const pageMenu =document.getElementById('page-menu');
+const pageMenu = document.getElementById('page-menu');
+const pageForm = document.getElementById('page-form');
+const pageAbout = document.getElementById('page-about');
+const pageMedia = document.getElementById('page-media');
+const pageFooter = document.getElementById('page-footer');
+
+//СТРАНИЦЫ
 
 
 buttonHeaderMenu.onclick = function(){
